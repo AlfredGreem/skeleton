79456dc610208dfcf07ed3b00dc19e431d23d09d
alfredgreem <houseofintellects@gmail.com>
2021-10-18 16:54:25 +0100

    categories section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Anouncements from '../components/Anouncements'
+import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
             <Anouncements/>
             <Navbar/>
             <Slider/>
+            <Categories/>
         </div>
     )
 }
